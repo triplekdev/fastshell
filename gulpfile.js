c2ce669d898c4c067999c994b2ba3c2e734fef4c
Kevy <tkralphdev@gmail.com>
2014-08-20 00:23:52 +0800

    Indention Fix
    Indention Fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp'),
-    less = require('gulp-less'),
-    browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
-    header = require('gulp-header'),
-    rename = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css'),
-    vendor = require('gulp-concat-vendor'),
-    rimraf = require('gulp-rimraf'),
-    package = require('./package.json');
+  less = require('gulp-less'),
+  browserSync = require('browser-sync'),
+  autoprefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  jshint = require('gulp-jshint'),
+  header = require('gulp-header'),
+  rename = require('gulp-rename'),
+  minifyCSS = require('gulp-minify-css'),
+  vendor = require('gulp-concat-vendor'),
+  rimraf = require('gulp-rimraf'),
+  package = require('./package.json');
 
 // Script Headers
-// 
+//
 var banner = [
   '/*!\n' +
   ' * <%= package.name %>\n' +
@@ -26,100 +26,100 @@ var banner = [
 ].join('');
 
 // Clean Javascripts
-// 
+//
 gulp.task('clean:js', function () {
-    gulp.src('app/assets/js/*', {
-        read: false
-    })
-        .pipe(rimraf({
-            force: true
-        }));
+  gulp.src('app/assets/js/*', {
+    read: false
+  })
+    .pipe(rimraf({
+      force: true
+    }));
 });
 
 // Clean Stylesheets
-// 
+//
 gulp.task('clean:css', function () {
-    gulp.src('app/assets/css/*', {
-        read: false
-    })
-        .pipe(rimraf({
-            force: true
-        }));
+  gulp.src('app/assets/css/*', {
+    read: false
+  })
+    .pipe(rimraf({
+      force: true
+    }));
 });
 
 // Compile Less
-// 
+//
 gulp.task('css', ['clean:css'], function () {
-    gulp.src('src/less/app.less')
-        .pipe(less({
-            paths: ['src/less/includes']
-        }))
-        .pipe(minifyCSS())
-        .pipe(header(banner, {
-            package: package
-        }))
-        .pipe(rename('app.min.css'))
-        .pipe(gulp.dest('app/assets/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+  gulp.src('src/less/app.less')
+    .pipe(less({
+      paths: ['src/less/includes']
+    }))
+    .pipe(minifyCSS())
+    .pipe(header(banner, {
+      package: package
+    }))
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Compile Javascripts
-// 
+//
 gulp.task('js', ['clean:js'], function () {
-    gulp.src('src/js/app.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-        .pipe(uglify())
-        .pipe(header(banner, {
-            package: package
-        }))
-        .pipe(rename('app.min.js'))
-        .pipe(gulp.dest('app/assets/js'))
-        .pipe(browserSync.reload({
-            stream: true,
-            once: true
-        }));
+  gulp.src('src/js/app.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(header(banner, {
+      package: package
+    }))
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({
+      stream: true,
+      once: true
+    }));
 });
 
 // Concat Vendor Scripts
-// 
+//
 gulp.task('vendor', function () {
-    gulp.src([
+  gulp.src([
       'src/vendor/*',
       'src/vendor/modernizr/modernizr.js'
     ])
-        .pipe(vendor('vendor.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/assets/js'))
-        .pipe(browserSync.reload({
-            stream: true,
-            once: true
-        }));
+    .pipe(vendor('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({
+      stream: true,
+      once: true
+    }));
 });
 
 // Server
-// 
+//
 gulp.task('browser-sync', function () {
-    browserSync.init(null, {
-        server: {
-            baseDir: "app"
-        }
-    });
+  browserSync.init(null, {
+    server: {
+      baseDir: "app"
+    }
+  });
 });
 
 // Reloading browser
-// 
+//
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 // Watchers
-// 
+//
 gulp.task('default', ['css', 'vendor', 'js', 'browser-sync'], function () {
-    gulp.watch("src/less/**/*.less", ['css']);
-    gulp.watch("src/vendor/**/*.js", ['vendor']);
-    gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("app/*.html", ['bs-reload']);
+  gulp.watch("src/less/**/*.less", ['css']);
+  gulp.watch("src/vendor/**/*.js", ['vendor']);
+  gulp.watch("src/js/*.js", ['js']);
+  gulp.watch("app/*.html", ['bs-reload']);
 });
